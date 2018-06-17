1c652f8a517579687900899cfa6ef0385a794b48
gamesir2 <241805907@qq.com>
2018-06-17 15:15:50 +0800

    增加策略
    增加策略，by李健勤
    
    Co-Authored-By: gamesir2 <gamesir2@users.noreply.github.com>

--- app.py
+++ app.py
@@ -12,15 +12,18 @@ from threading import Thread
 from balance import balance
 import json
 
+
+
 class app():
     def __init__(self):
         self.fcoin = Fcoin()
         self.fcoin.auth(config.api_key, config.api_secret)
       
-        self.symbol = 'ethusdt'
+        self.symbol = 'ftusdt'
         self.order_id = None
         self.dic_balance = defaultdict(lambda: None)
         self.time_order = time.time()
+        self.oldprice = 0.0
 
     def digits(self, num, digit):
         site = pow(10, digit)
@@ -46,31 +49,35 @@ class app():
     def process(self):
         self.dic_balance = self.get_blance()
  
-        eth = self.dic_balance['eth']
+        ft = self.dic_balance['ft']
     
         usdt = self.dic_balance['usdt']
-        print('usdt  has ....', usdt.available, 'eth has ...', eth.available)
-        price = self.digits(self.get_ticker(),2)
+        print('usdt  has ....', usdt.available, 'ft has ...', ft.available)
+        price = self.digits(self.get_ticker(),6)
        
         order_list = self.fcoin.list_orders(symbol=self.symbol,states='submitted')['data']
 
         print('order list',order_list)
         if not order_list or len(order_list) < 3:
             if usdt:
-                amount = self.digits(usdt.available / price * 0.2, 4)
-                if amount > 0.01:
-                    data = self.fcoin.buy(self.symbol, price, amount)
-                    if data:
-                        print('buy success',data)
-                        self.order_id = data['data']
-                        self.time_order = time.time()
-                if  float(eth.available) * 0.2 > 0.01:
-                    amount = self.digits(eth.available * 0.2, 4)
-                    data = self.fcoin.sell(self.symbol, price, amount)  
-                    if data:
-                        self.time_order = time.time()
-                        self.order_id = data['data']
-                        print('sell success')
+                if price>self.oldprice:
+                    amount = self.digits(usdt.available / price * 0.25, 6)
+                    if amount > 5:
+                        data = self.fcoin.buy(self.symbol, price, amount)
+                        if data:
+                            print('buy success',data)
+                            self.oldprice=price
+                            self.order_id = data['data']
+                            self.time_order = time.time()
+                else:
+                    if  float(ft.available) * 0.25 > 5:
+                        amount = self.digits(ft.available * 0.25, 6)
+                        data = self.fcoin.sell(self.symbol, price, amount)  
+                        if data:
+                            self.oldprice=price
+                            self.time_order = time.time()
+                            self.order_id = data['data']
+                            print('sell success')
             else:
                 print('error')
         else:
