399c7f075808a6161486f275dae64a9e373fbb01
gamesir2 <241805907@qq.com>
2018-06-20 17:43:00 +0800

    优化过程，以及在config内增加交易对设置

--- app.py
+++ app.py
@@ -16,15 +16,23 @@ class app():
     def __init__(self):
         self.fcoin = Fcoin()
         self.fcoin.auth(config.api_key, config.api_secret)
-
-        self.symbol = 'ftusdt'
+        self.symbol = config.socket+config.blc
         self.order_id = None
         self.dic_balance = defaultdict(lambda: None)
         self.time_order = time.time()
-        self.oldprice = [self.digits(self.get_ticker(),6)]
-        self.usdt_sxf=0.0
-        self.ft_sxf=0.0
+        self.oldprice = self.get_prices()
+        self.socket_sxf=0.0
+        self.blc_sxf=0.0
         self.begin_balance=self.get_blance()
+        
+    def get_prices(self):
+        i=2
+        prices=[]
+        while i>0:
+           prices.append(self.digits(self.get_ticker(),6))
+           i-=1
+           time.sleep(1)
+        return prices
 
     def digits(self, num, digit):
         site = pow(10, digit)
@@ -51,36 +59,39 @@ class app():
         
         self.oldprice.append(price)
         
+        p1=self.oldprice[len(self.oldprice)-2]
+        p2=self.oldprice[len(self.oldprice)-3]
+        
         self.dic_balance = self.get_blance()
 
-        ft = self.dic_balance['ft']
+        socket = self.dic_balance[config.socket]
 
-        usdt = self.dic_balance['usdt']  
+        blc = self.dic_balance[config.blc]  
         
-        print('usdt_now  has ....', usdt.balance, 'ft_now has ...', ft.balance)
-        print('usdt_sxf  has ....', self.usdt_sxf, 'ft_sxf has ...', self.ft_sxf)
-        print('usdt_begin  has ....', self.begin_balance['usdt'].balance, 'ft_begin has ...', self.begin_balance['ft'].balance)
-        print('usdt_all_now  has ....', usdt.balance+self.usdt_sxf, 'ft_all_now has ...', ft.balance+self.ft_sxf)
+        print(config.blc+'_now  has ....', blc.balance, config.socket+'_now has ...', socket.balance)
+        print(config.blc+'_sxf  has ....', self.blc_sxf, config.socket+'_sxf has ...', self.socket_sxf)
+        print(config.blc+'_begin  has ....', self.begin_balance[config.blc].balance, config.socket+'_begin has ...', self.begin_balance[config.socket].balance)
+        print(config.blc+'_all_now  has ....', blc.balance+self.blc_sxf, config.socket+'_all_now has ...', socket.balance+self.socket_sxf)
 
         order_list = self.fcoin.list_orders(symbol=self.symbol,states='submitted')['data'] 
 
         if not order_list or len(order_list) < 2:
-            if usdt and abs(price/self.oldprice[len(self.oldprice)-2]-1)<0.02:
-                if price*2>self.oldprice[len(self.oldprice)-2]+self.oldprice[len(self.oldprice)-3]:
-                    amount = self.digits(usdt.available / price * 0.25, 2)
+            if blc and abs(price/self.oldprice[len(self.oldprice)-2]-1)<0.02:
+                if price*2>p1+p2:
+                    amount = self.digits(blc.available / price * 0.25, 2)
                     if amount > 5:
                         data = self.fcoin.buy(self.symbol, price, amount)
                         if data:
                             print('buy success',data)
-                            self.ft_sxf += amount*0.001
+                            self.socket_sxf += amount*0.001
                             self.order_id = data['data']
                             self.time_order = time.time()
                 else:
-                    if  float(ft.available) * 0.25 > 5:
-                        amount = self.digits(ft.available * 0.25, 2)
+                    if  float(socket.available) * 0.25 > 5:
+                        amount = self.digits(socket.available * 0.25, 2)
                         data = self.fcoin.sell(self.symbol, price, amount)
                         if data:
-                            self.usdt_sxf += amount*price*0.001
+                            self.blc_sxf += amount*price*0.001
                             self.time_order = time.time()
                             self.order_id = data['data']
                             print('sell success')
@@ -92,10 +103,10 @@ class app():
                 data=self.fcoin.cancel_order(order_list[len(order_list)-1]['id'])
                 print(order_list[len(order_list)-1])
                 if data:
-                    if order_list[len(order_list)-1]['side'] == 'buy' and order_list[len(order_list)-1]['symbol'] == 'ftusdt':
-                        self.ft_sxf -= float(order_list[len(order_list)-1]['amount'])*0.001
-                    elif order_list[len(order_list)-1]['side'] == 'sell' and order_list[len(order_list)-1]['symbol'] == 'ftusdt':
-                        self.usdt_sxf -= float(order_list[len(order_list)-1]['amount'])*float(order_list[len(order_list)-1]['price'])*0.001
+                    if order_list[len(order_list)-1]['side'] == 'buy' and order_list[len(order_list)-1]['symbol'] == self.symbol:
+                        self.socket._sxf -= float(order_list[len(order_list)-1]['amount'])*0.001
+                    elif order_list[len(order_list)-1]['side'] == 'sell' and order_list[len(order_list)-1]['symbol'] == self.symbol:
+                        self.blc_sxf -= float(order_list[len(order_list)-1]['amount'])*float(order_list[len(order_list)-1]['price'])*0.001
         
         
 
